c7d1371056cc164c7972de71d18d6fb9463534ba
Benqx00 <thomazcandelaria58@gmail.com>
2026-08-04 17:05:09 -0300

    exercicio 2

--- Aula-03/Exercicios/Exercicio2/Program.cs
+++ Aula-03/Exercicios/Exercicio2/Program.cs
@@ -2,6 +2,39 @@
 {
     private static void Main(string[] args)
     {
-        
+        int num2;
+        int num1;
+
+        Console.WriteLine("digite o primeiro numero: ");
+        num1 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("1 - soma");
+        Console.WriteLine("2 - subtração");
+        Console.WriteLine("3 - multiplicação");
+        Console.WriteLine("4 - divisão");
+        string operacao;
+        Console.WriteLine("selecione uma operacao: ");
+        operacao = Console.ReadLine();
+
+        switch (operacao)
+        {
+            case "1": 
+                Console.WriteLine(num1 + num2);
+                break;
+            case "2": 
+                Console.WriteLine(num1 - num2);
+                break;
+            case "3": 
+                Console.WriteLine(num1 * num2);
+                break;
+            case "4": 
+                Console.WriteLine(num1 / num2);
+                break;
+            default:
+            Console.WriteLine("operador informado nao é valido!")
+                break;
+        }
+
+    
     }
 }
