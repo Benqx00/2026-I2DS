ec77272084f7225959401c931c937106e6d2bacf
Benqx00 <thomazcandelaria58@gmail.com>
2026-08-04 16:44:17 -0300

    exercicio1

---
+++ Aula-03/Exercicios/Exercicio1/Program.cs
@@ -0,0 +1,13 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double celsius;
+        double fahrenheit;
+        Console.WriteLine("digite a temperatura em celsius: ");
+        celsius = double.Parse(Console.ReadLine());
+        fahrenheit = (celsius * 9 / 5) + 32;
+        Console.WriteLine("a temperatura em fahrenheit é: " + fahrenheit);
+
+    }
+}
