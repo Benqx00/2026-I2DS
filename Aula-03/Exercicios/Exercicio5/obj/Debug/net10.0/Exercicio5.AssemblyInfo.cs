b4b8a6353181a145669d8a2c129be7d0aeb2107b
Benqx00 <thomazcandelaria58@gmail.com>
2026-08-05 09:18:39 -0300

    exercicio 10 ,9, 7,6 e 5 feitos

--- Aula-03/Exercicios/Exercicio5/obj/Debug/net10.0/Exercicio5.AssemblyInfo.cs
+++ Aula-03/Exercicios/Exercicio5/obj/Debug/net10.0/Exercicio5.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exercicio5")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7d1371056cc164c7972de71d18d6fb9463534ba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06378c55f0d89ad4e2a51778f7f6c25b1b13729c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exercicio5")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exercicio5")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Aula-03/Exercicios/Exercicio6/Program.cs
+++ Aula-03/Exercicios/Exercicio6/Program.cs
@@ -1,1 +1,32 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string resposta = "S";
+
+        while (resposta == "S" || resposta == "s")
+        {
+            Console.Write("Digite um número: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            MostrarTabuada(numero);
+
+            Console.Write("Deseja continuar? (S/N): ");
+            resposta = Console.ReadLine();
+
+            Console.WriteLine();
+        }
+
+        Console.WriteLine("Programa encerrado.");
+    }
+
+    static void MostrarTabuada(int numero)
+    {
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.WriteLine(numero + " x " + i + " = " + (numero * i));
+        }
+    }
+}
